236d0df9d9f888a46179a452e033d3758f22d7cc
Patrick Fung <patrick@makestuffdostuff.com>
2016-02-23 16:39:22 -0500

    configure sshd security using cookbook

--- cookbooks/lemp_webserver/metadata.rb
+++ cookbooks/lemp_webserver/metadata.rb
@@ -2,6 +2,8 @@ name             'lemp_webserver'
 maintainer       'Patrick Fung'
 maintainer_email 'patrick@makestuffdostuff.com'
 license          'All rights reserved'
-description      'Configures a base VM for installing applications'
+description      'Installs and configures services required for a basic webserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'sshd', '~> 1.1.3'
--- cookbooks/lemp_webserver/recipes/default.rb
+++ cookbooks/lemp_webserver/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# Configure sshd: Disable password authentication and root login
+openssh_server node['sshd']['config_file'] do
+  cookbook 'lemp_webserver'
+  source 'sshd_config.erb'
+end
