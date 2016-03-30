c46d28a530980a1acd48fca567030f7645b9d61a
Patrick Fung <patrick@makestuffdostuff.com>
2016-03-30 14:45:13 -0400

    reworking how the cookbooks work together

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,9 @@
 Vagrant.configure(2) do |config|
   # Configuration variables for this VM
   conf = {
-    vm_box: 'ubuntu/trusty64', 
+    vm_box: 'ubuntu/trusty64',
     private_network_ip: '192.168.33.10',
-    vm_hostname: 'vagrant.dev', 
+    vm_hostname: 'vagrant.dev',
     vm_memory: 4096,
     vm_cpus: 2
   }
@@ -46,6 +46,6 @@ Vagrant.configure(2) do |config|
     }
 
     chef.add_recipe 'lemp_webserver'
-    chef.add_recipe 'kirby'
+    # chef.add_recipe 'kirby'
   end
 end
--- cookbooks/lemp_webserver/recipes/default.rb
+++ cookbooks/lemp_webserver/recipes/default.rb
@@ -11,43 +11,43 @@ package 'git'
 include_recipe 'php'
 include_recipe 'composer'
 
-# install nginx
-include_recipe 'nginx'
+# # install nginx
+# include_recipe 'nginx'
 
-# Create and start MySQL instance
-mysql_service 'default' do
-  bind_address '127.0.0.1'
-  version '5.6'
-  initial_root_password 'please change me'
-  action [:create, :start]
-  not_if { node['lemp_webserver']['skip_mysql'] }
-end
+# # Create and start MySQL instance
+# mysql_service 'default' do
+#   bind_address '127.0.0.1'
+#   version '5.6'
+#   initial_root_password 'please change me'
+#   action [:create, :start]
+#   not_if { node['lemp_webserver']['skip_mysql'] }
+# end
 
-# Configure sshd: Disable password authentication and root login
-openssh_server node['sshd']['config_file'] do
-  cookbook 'lemp_webserver'
-  source 'sshd_config.erb'
-end
+# # Configure sshd: Disable password authentication and root login
+# openssh_server node['sshd']['config_file'] do
+#   cookbook 'lemp_webserver'
+#   source 'sshd_config.erb'
+# end
 
-# Configure firewall
-firewall 'default' do
-  action :install
-end
+# # Configure firewall
+# firewall 'default' do
+#   action :install
+# end
 
-# Allow SSH so we don't lock ourselves out
-firewall_rule 'ssh' do
-  port     22
-  command  :allow
-  action :create
-end
+# # Allow SSH so we don't lock ourselves out
+# firewall_rule 'ssh' do
+#   port     22
+#   command  :allow
+#   action :create
+# end
 
-# Allow HTTP
-firewall_rule 'http' do
-  port     80
-  command  :allow
-  action :create
-end
+# # Allow HTTP
+# firewall_rule 'http' do
+#   port     80
+#   command  :allow
+#   action :create
+# end
 
-php_fpm_pool 'default' do
-  action :install
-end
+# php_fpm_pool 'default' do
+#   action :install
+# end
