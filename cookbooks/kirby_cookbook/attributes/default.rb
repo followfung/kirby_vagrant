6f9f491115de0f79591e6d4a9fbb41ecf04deff2
Patrick Fung <patrick@makestuffdostuff.com>
2016-03-31 11:04:42 -0400

    break kirby cli stuff into it's own cookbook

--- cookbooks/kirby_cookbook/attributes/default.rb
+++ cookbooks/kirby_cookbook/attributes/default.rb
@@ -3,11 +3,6 @@
 ### Seriously, consider yourself warned.                 ###
 ############################################################
 
-# Kirby CLI stuff
-default['kirby']['cli']['git_repo'] = 'git://github.com/getkirby/cli.git'
-default['kirby']['cli']['src_path'] = '/usr/local/src/kirby_cli'
-default['kirby']['cli']['bin_path'] = '/usr/local/bin'
-
 default['kirby']['log_dir'] = "#{node['nginx']['log_dir']}/kirby"
 default['kirby']['default_root'] = "#{node['nginx']['default_root']}/kirby"
 default['kirby']['server_name'] = 'localhost'
--- cookbooks/kirby_cookbook/recipes/default.rb
+++ cookbooks/kirby_cookbook/recipes/default.rb
@@ -4,18 +4,8 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-# Clone down Kirby CLI repo from GitHub
-git node['kirby']['cli']['src_path'] do
-  repository node['kirby']['cli']['git_repo']
-end
-link "#{node['kirby']['cli']['bin_path']}/kirby" do
-  to "#{node['kirby']['cli']['src_path']}/kirby"
-end
-execute 'Install Kirby CLI' do
-  cwd node['kirby']['cli']['src_path']
-  command 'composer install'
-  creates "#{node['kirby']['cli']['src_path']}/composer.lock"
-end
+# Install Kirby CLI
+include_recipe 'kirby_cli'
 
 # install and configure dependencies
 include_recipe 'kirby::nginx'
