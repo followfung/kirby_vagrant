2ecfb2810325998cb541efe58ed993800ef076bf
Patrick Fung <patrick@makestuffdostuff.com>
2016-04-01 11:02:02 -0400

    delegate user that runs kirby cli commands to the one defined in nginx cookbook

--- cookbooks/kirby_cookbook/attributes/default.rb
+++ cookbooks/kirby_cookbook/attributes/default.rb
@@ -9,6 +9,3 @@ default['kirby']['server_name'] = 'localhost'
 default['kirby_cli']['git_repo'] = 'git://github.com/getkirby/cli.git'
 default['kirby_cli']['src_path'] = '/usr/local/src/kirby_cli'
 default['kirby_cli']['bin_path'] = '/usr/local/bin'
-
-default['kirby_cli']['user'] = node['nginx']['user'] || node['kirby_cli']['user']
-default['kirby_cli']['group'] = node['nginx']['group'] || node['kirby_cli']['group']
